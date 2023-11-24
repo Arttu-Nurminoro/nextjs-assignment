7dc97996cb85016747aa5ecb94f0764b87c17ea8
Arttu <earnu002@edu.xamk.fi>
2023-11-24 13:39:01 +0200

    lisää turhaa tyylitystä

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 import Navbar from './components/Navbar';
 import Chartti from './components/Chart';
 export default async function Home() {
-  
+
   return (
     <main className="flex flex-col h-full w-auto">
       <Navbar>
-        <Link href='/PricesList'>Prices</Link>
+        <Link className='m-auto' href='/PricesList'>Prices</Link>
       </Navbar>
-      <div className='flex flex-col w-auto h-auto p-5'>
-        <p className=''>Jotai tekstii jotai tekstii</p>
+      <div className='flex flex-col w-auto h-auto m-auto'>
+        <p>Jotai tekstii jotai tekstii</p>
         <h1>isodhfsuisdhfdsjfdsdfos</h1>
       </div>
       <Chartti/>
