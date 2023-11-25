f0949bc089c17a43caf61c1b2e5524d8cf601b8a
Arttu <earnu002@edu.xamk.fi>
2023-11-25 13:17:30 +0200

    fdhsfihsoidflhsuhfudoi

--- src/app/PricesList/page.tsx
+++ src/app/PricesList/page.tsx
@@ -1,21 +1,14 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Navbar from "../components/Navbar"
 import Link from "next/link"
-export default function page() {
+
+export default async function page() {              //https://api.porssisahko.net/v1/price.json?date=[date]&hour=[hour]
 
     return(
         <main>
             <Navbar>
                 <Link href='/PricesList'>Prices</Link>
             </Navbar>
-            <div>
-                <p></p>
-            </div>
-            <div>
-                <ul>
-                    {}
-                </ul>
-            </div>
         </main>
-    )
+    );
 }
