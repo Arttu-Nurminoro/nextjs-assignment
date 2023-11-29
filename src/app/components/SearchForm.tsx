503fdf905012fe09e00e2fe6ffa02284991aeb0c
Arttu <earnu002@edu.xamk.fi>
2023-11-29 09:07:22 +0200

    efdssrhjhgkgiyutyerterfdbghgjkgjytrtwe

--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -5,6 +5,7 @@ import DatePicker from 'react-date-picker';
 
 type ValuePiece = Date | null;
 type value = ValuePiece | [ValuePiece, ValuePiece]
+
 export default function SearchForm() {
     const [SearchDate, setSearchDate] = useState<value>(new Date());    //css stylet seuraavaks ja toi joku data hankinta paska et joooooooooooooo ja si pitäs tehä se jälkee se yks jutu näkymä
     function JokuSiisti(tempstring:string) {
@@ -17,10 +18,12 @@ export default function SearchForm() {
     tomorrow.setDate(tomorrow.getDate()+1);
     const maxDate = tomorrow;
     return(
-        <div className=" bg-white flex flex-col">
-            <p>Anna päivä jolta haluat hakea sähkön hintoja </p>
-            <DatePicker minDate={minDate} maxDate={maxDate} required={true} locale='fi-FI' value={SearchDate} returnValue="start" onChange={e => {setSearchDate(old => e); console.log(e)}} />
-            <button type="button">Search</button>
+        <div className=" bg-white flex flex-col border border-solid border-black p-2 w-auto h-4/5 justify-normal">
+            <p className=''>Anna päivä jolta haluat hakea sähkön hintoja </p>
+            <div className='flex flex-row justify-normal grid-rows-2 max-w-fit'>
+                <button type="button" className='p-2 border border-gray-500 hover:shadow-inner max-w-fit hover:text-blue-400'>Search</button>
+                <DatePicker className="max-w-fit" minDate={minDate} maxDate={maxDate} required={true} locale='fi-FI' value={SearchDate} returnValue="start" onChange={e => {setSearchDate(old => e); console.log(e)}} />
+            </div>
         </div>
     );
 }
