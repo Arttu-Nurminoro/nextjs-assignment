48bd5bab504b1cf7d31f14e70b271766dde35567
Arttu-Nurminoro <arttunurminoro@gmail.com>
2023-12-14 18:58:42 +0200

    ei oo motii tä roskan työstämisee

--- src/app/PricesList/page.tsx
+++ src/app/PricesList/page.tsx
@@ -1,3 +1,4 @@
+import { Moment } from "moment";
 import Link from "next/link";
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
@@ -5,12 +6,13 @@ import 'react-datetime-picker/dist/DateTimePicker.css';
 import Navbar from "../components/Navbar";
 import SearchForm from "../components/SearchForm";
 
-async function HaeJuttuu() {
-    // const Dataa = await fetch("")
+async function HaeJuttuu(moment:Moment) {
+    const Dataa = await fetch(`https://api.porssisahko.net/v1/price.json?date=${moment.format("YYYY-MM-DD")}&hour=${moment.hour()}`);
+    return Dataa.json()
 }
 
 export default async function page() {              //https://api.porssisahko.net/v1/price.json?date=[date]&hour=[hour]
-    
+
     return(
         <main className='flex flex-col w-auto h-auto'>
             <Navbar>
--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -1,22 +1,38 @@
 'use client'
 import moment, { Moment } from 'moment';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DatePicker from 'react-datetime-picker';
 
 
 export default function SearchForm() {
-    const [SearchDate, setSearchDate] = useState<Moment>(moment(new Date()));
+    const [SearchDate, setSearchDate] = useState<Date>(new Date());
+    const [Data, setData] = useState([]);
+    const [HasError, setHasError] = useState<Boolean>();
     const minDate = new Date("2021-01-01");
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate()+1);
     const maxDate = tomorrow;
+    var pvm:any = SearchDate;
+    pvm = pvm.toISOString().split("T", 1)
+    var hour:any = SearchDate;
+    hour = SearchDate.getHours();
+    console.log(pvm, hour);
+    useEffect(() => {
+        fetch(`https://api.porssisahko.net/v1/price.json?date=${pvm[0]}&hour=${hour}`, {mode:"no-cors"})
+        .then(res => res.json())
+        .then(data => setData(data))
+        .catch(err => {setHasError(true); console.log(err)})
+    }, )
+    console.log(Data)
     return(
         <div className=" bg-white flex flex-col border border-solid border-black p-2 w-auto h-4/5 justify-normal">
             <p className=''>Hae yksittäisen tunnin hinta.</p>
             <div className='flex flex-row justify-normal grid-rows-2 max-w-fit'>
-                <DatePicker className="max-w-fit" minDate={minDate} maxDate={maxDate} disableClock={true} required={true} locale='fi-FI' returnValue="start" value={SearchDate.toString()} onChange={e => {setSearchDate(old => e); console.log(e)}} />
+                <DatePicker className="max-w-fit" minDate={minDate} maxDate={maxDate} value={SearchDate.toString()} disableClock={true} required={true} locale='fi-FI' returnValue="start" onChange={(e) => {setSearchDate(old => e)}} />
+            </div>
+            <div>
+                {/* <p>(typeof Data !== undefined)? : {Data.price}</p> */}
             </div>
         </div>
     );
 }
-
